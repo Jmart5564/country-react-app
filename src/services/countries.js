d27340d84f4c728da2e4e3c057e87615f5bdb238
Jmart5564 <Jmart5564@gmail.com>
2022-09-20 20:56:06 -0700

    added name of database to fetchCountries function

--- src/services/countries.js
+++ src/services/countries.js
@@ -2,6 +2,7 @@ import { checkError, client } from './client';
 
 export async function fetchCountries() {
   const resp = await client
+    .from('countries')
     .select('*')
 
     return checkError(resp);
